db582992f1f82b3e2fce97f06fb2e4c7c94d8f37
pino._.hoo <inhoo23@naver.com>
2022-09-15 02:47:29 +0900

    test 3

--- src/stamp/infrastructure/stamp.repository.ts
+++ src/stamp/infrastructure/stamp.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm'
-import { Stamp } from '../domain/Stmap.entity'
+import { Stamp } from '../domain/stmap.entity'
 
 @EntityRepository(Stamp)
 export class StampRepository extends Repository<Stamp> {}
