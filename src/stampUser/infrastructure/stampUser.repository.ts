bab08a327d64c7334bac3e32a7d3b087fa1101b3
pino._.hoo <inhoo23@naver.com>
2022-09-29 03:31:49 +0900

    remove : stampUser API 삭제

--- src/stampUser/infrastructure/stampUser.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { StampUser } from '../domain/stampUser.entity'
-
-@EntityRepository(StampUser)
-export class StampUserRepository extends Repository<StampUser> {}
