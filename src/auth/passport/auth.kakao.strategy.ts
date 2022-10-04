14415ae254f14db87a0e9d49e325306da4512689
pino._.hoo <inhoo23@naver.com>
2022-10-05 02:20:00 +0900

    fix : kakao callback

--- src/auth/passport/auth.kakao.strategy.ts
+++ src/auth/passport/auth.kakao.strategy.ts
@@ -5,7 +5,7 @@ export class KakaoStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       clientID: '35925e2699a55c7bffbb6e051da01a40',
-      callbackURL: 'http://210.90.136.10:3002/auth/kakao/callback',
+      callbackURL: 'http://54.197.28.78:8080/auth/kakao/callback',
       // callbackURL: 'http://localhost:3002/auth/kakao/callback',
     })
   }
