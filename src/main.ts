4036e1281cdc87d3596addf1231411e2b023ed14
pino._.hoo <inhoo23@naver.com>
2022-10-20 15:53:59 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,26 @@ import * as Sentry from '@sentry/node'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  })
-  app.useGlobalPipes(new ValidationPipe())
-  app.use(cookieParser())
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-  })
-  await app.listen(process.env.NODE_SERVER_PORT || 443)
+  try {
+    const app = await NestFactory.create(AppModule)
+    app.enableCors({
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    })
+    app.useGlobalPipes(new ValidationPipe())
+    app.use(cookieParser())
+
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+    })
+    console.log(process.env.NODE_SERVER_PORT)
+    await app.listen(8080)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 bootstrap()
